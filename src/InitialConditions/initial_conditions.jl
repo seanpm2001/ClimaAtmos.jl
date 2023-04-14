903d6a53f269b493346a969e1b334924628107b2
Valeria Barra <valeriabarra21@gmail.com>
2023-04-14 15:08:03 -0700

    Fix FT in initial q_0 and q_t

--- src/InitialConditions/initial_conditions.jl
+++ src/InitialConditions/initial_conditions.jl
@@ -242,8 +242,8 @@ function moist_baroclinic_wave_values(z, ϕ, λ, params, perturb)
     # Constants from paper
     p_w = FT(3.4e4)
     p_t = FT(1e4)
-    q_t = FT(1e-12) * 0.0
-    q_0 = FT(0.018) * 0.01
+    q_t = FT(1e-12) * FT(0.0)
+    q_0 = FT(0.018) * FT(0.01)
     ϕ_w = FT(40)
     ε = FT(0.608)
 
